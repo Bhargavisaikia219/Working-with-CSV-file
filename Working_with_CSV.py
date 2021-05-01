9dcb6417db3513b3638bc19909f0887f5bbe8369
Bhargavi Saikia <bhargavisaikia187@gmail.com>
2021-05-01 12:02:08 +0530

    concatenate climate data and yields of apples, print outputs of each calculation

--- Working_with_CSV.py
+++ Working_with_CSV.py
@@ -4,10 +4,19 @@ import urllib.request
 urllib.request.urlretrieve("https://raw.githubusercontent.com/Bhargavisaikia219/CSV_files/main/climate.csv","climate.txt")
 
 climate_data = np.genfromtxt('climate.txt', delimiter=',', skip_header=1)
+print("\nData from the file are given below:\n")
+print(climate_data)
 
 w1, w2, w3 = 0.3, 0.2, 0.5
-weights=np.array([w1, w2, w3])
+weights = np.array([w1, w2, w3])
+print("\nWeights given are:\n")
 print(weights)
 
-yields= climate_data @ weights
-print(yields)
+yields_of_apples = climate_data @ weights
+print("\nYields of apples based on climatic conditions:\n")
+print(yields_of_apples)
+
+climate_data_final = np.concatenate((climate_data, yields_of_apples.reshape(10000,1)), axis=1)
+print("\nFinal data:\n")
+print(climate_data_final)
+
